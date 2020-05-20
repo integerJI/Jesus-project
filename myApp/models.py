10e6354e3029f7866f6a392fb4afd90e8e556ede
integerJI <jjs9536@gmail.com>
2020-05-20 21:55:54 +0900

    버튼을 눌렀을때 시간 저장하는 기능 개발중

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Save(models.Model):
+    save_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    save_date = models.DateTimeField('date published', null = True, default=timezone.now)
+
+    def __str__(self):
+        return '%s - %s' % (self.save_user, self.save_date) 
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,18 @@
 # myProject/myApp/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.utils import timezone
+from .models import Save
 
 def index(request):
+    saves = Save.objects.all().order_by('-id')
+    print(saves)
+    return render(request, 'index.html', {'saves':saves})
+
+def save(request):
+    save = Save()
+    save.save_user = User.objects.get(username = request.user.get_username())
+    save.save_date = timezone.datetime.now()
+    save.save()
     return render(request, 'index.html')
