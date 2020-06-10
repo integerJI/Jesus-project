8b2a8b75be5c38305e36a4abf746cc83c64b4995
integerJI <jjs9536@gmail.com>
2020-06-10 09:22:25 +0900

    여러가지 디자인 수저저

--- myApp/views.py
+++ myApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Save
+from myMember.models import Profile
 
 def index(request):
     request_user = request.user
@@ -21,4 +22,15 @@ def calender(request):
 
 # 정수 전용 페이지
 def dev(request):
-    return render(request, 'dev.html')
+    request_user = request.user
+    saves = Save.objects.all().filter(save_user_id=request_user).order_by('-id')
+    request_profile = Profile.objects.get(user=request_user)
+
+    context = {
+       'id' : request_user.username,
+       'nick' : request_profile.nick,
+       'intro' : request_profile.intro,
+       'saves' : saves,
+    }
+
+    return render(request, 'dev.html', context=context)
